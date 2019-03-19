1c9948cabb856bceb78a9ad5aac47fab3807dd74
saikumar <saikumar.kusuma@gmail.com>
2019-03-19 21:26:28 +0530

    chat in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChatModule} from './chat/chat.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChatModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,16 +1,19 @@
 import {CommonModule} from '@angular/common';
-import {Injector, NgModule} from '@angular/core';
+import {Injector, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {createCustomElement} from '@angular/elements';
 import {ReactiveFormsModule} from '@angular/forms';
 
+import {ChatDescriptionComponent} from './chat-description/chat-description.component';
+import {ChatStackComponent} from './chat-stack/chat-stack.component';
 import {ChatComponent} from './chat/chat.component';
-import { ChatStackComponent } from './chat-stack/chat-stack.component';
-import { ChatDescriptionComponent } from './chat-description/chat-description.component';
+import {ChatAdapterService} from './services/chat-adapter.service';
 
 @NgModule({
   declarations: [ChatComponent, ChatStackComponent, ChatDescriptionComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  entryComponents: [ChatComponent]
+  entryComponents: [ChatComponent],
+  providers: [ChatAdapterService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ChatModule {
   constructor(injector: Injector) {
